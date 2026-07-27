28e91aadb06aa088929b38572d0ca625b5d930ee
tokyorozuki-blip <to.kyorozuki@gmail.com>
2026-07-28 01:32:19 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: 'es2020',
+  },
   build: {
-    target: 'esnext',
+    target: 'es2020',
   },
 });
